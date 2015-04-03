8915eb5c81a10a90ef4fff09e695affcbb87073c
zhh007 <zhh9113@hotmail.com>
2015-04-03 16:27:09 +0800

    图片截取大小不固定

---
+++ MvcFox/MvcFox/Controllers/ImageEditController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcFox.Controllers
+{
+    public class ImageEditController : Controller
+    {
+        // GET: ImageEdit
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
