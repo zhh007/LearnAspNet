553fae618055b8e61bb1e66b8417156d36960c83
zhh007 <zhh9113@hotmail.com>
2015-08-26 11:59:58 +0800

    modify jquery ui style

---
+++ MvcFox/MvcFox/Controllers/TestDialogController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcFox.Controllers
+{
+    public class TestDialogController : Controller
+    {
+        // GET: TestDialog
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
