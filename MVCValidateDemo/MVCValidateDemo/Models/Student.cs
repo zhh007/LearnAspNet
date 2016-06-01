02f531b0fc16aea440aa4fdf983d630e9cf5f834
zhh007 <zhh9113@hotmail.com>
2016-06-01 19:43:40 +0800

    普通表单验证

---
+++ MVCValidateDemo/MVCValidateDemo/Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCValidateDemo.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        [Required]
+        [DisplayName("学生姓名")]
+        public string Name { get; set; }
+
+        [Required]
+        [DisplayName("年龄")]
+        public int Age { get; set; }
+    }
+}
