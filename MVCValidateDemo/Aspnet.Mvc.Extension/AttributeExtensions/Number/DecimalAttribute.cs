f109a9453212ebc8d215cc21d997f00de931823f
zhh007 <zhh9113@hotmail.com>
2016-06-21 15:54:53 +0800

    update

---
+++ MVCValidateDemo/Aspnet.Mvc.Extension/AttributeExtensions/Number/DecimalAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aspnet.Mvc.Extension
+{
+    /// <summary>
+    /// 小数
+    /// </summary>
+    public class DecimalAttribute
+    {
+
+    }
+}
